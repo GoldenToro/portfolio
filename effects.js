4e01fcc9a904411f41f96a51bddac78ba9a790fe
GoldenToro <max.goeldnernew@gmail.com>
2024-02-11 20:02:34 +0100

    working fullsize effect

--- effects.js
+++ effects.js
@@ -1,6 +1,8 @@
 /* -- Glow effect -- */
 const blob = document.getElementById("blob");
 
+var scrollBefore = 0;
+
 window.onpointermove = event => {
   const { clientX, clientY } = event;
 
@@ -9,8 +11,6 @@ window.onpointermove = event => {
     top: `${clientY}px`
   }, { duration: 30000, fill: "forwards" });
 };
-
-
 
 function updateCardEffects() {
 
@@ -21,8 +21,7 @@ function updateCardEffects() {
       .on("mousemove touchmove", function(e) {
 
         var card = $(this);
-        if (!(card.hasClass('wait') || card.hasClass('card-active') || card.hasClass('big') )) {
-
+        if (!(card.hasClass('wait') || card.hasClass('card-active') || card.hasClass('big') || card.hasClass('absolute') )) {
 
             // normalise touch/mouse
             var pos = [e.offsetX,e.offsetY];
@@ -55,14 +54,18 @@ function updateCardEffects() {
       })
       .on("mouseout touchend touchcancel", function() {
         // remove css, apply custom animation on end
-        removeStyle($(this));
+        if (!($(this).hasClass('absolute'))) {
+            removeStyle($(this));
+
+       }
 
       })
       .on("click",function(){
 
         var card = $(this);
+        var container = card.closest('.card-container');
 
-        if (!card.hasClass('card-active')) {
+        if (!(card.hasClass('card-active') || container.hasClass('active'))) {
             turnAround(card);
         }
       });
@@ -72,17 +75,38 @@ function updateCardEffects() {
         e.stopPropagation();
         var card = $(this).closest('.card');
 
+        var container = card.closest('.card-container');
 
-        turnAround(card);
+        if (!container.hasClass('active')) {
+            turnAround(card);
+        }
 
     });
 
     $('.more').on("click",function(e){
         e.stopPropagation();
 
-
         var card = $(this).closest('.card');
-        toggleFullSize(card)
+
+        var timeOut = 900;
+
+        if (card.hasClass("big")) {
+            timeOut = 1800;
+        }
+
+        if (!card.hasClass('active')) {
+
+            $(".card").addClass('active');
+
+            toggleFullSize(card);
+
+            setTimeout(() => {
+                $(".card").removeClass('active');
+            }, timeOut);
+
+        }
+
+
 
     });
 
@@ -139,39 +163,116 @@ function toggleFullSize(card) {
 
     var container = card.closest('.card-container');
 
-    var sourcePosition = card.position();
-    var sourceWidth = card.width();
-    var sourceHeight = card.height();
+    var isFullsize = container.hasClass("big");
 
-    console.log(sourcePosition)
-
-    $(".card").toggleClass("big");
-
-    card.toggleClass("absolute");
-
-    setTimeout(() => {
-        card.nextAll().toggleClass("right")
-        card.prevAll().toggleClass("left")
-    }, 100);
-
-    card.css({
-      top: sourcePosition.top + 'px',
-      left: sourcePosition.left + 'px',
-      width: sourceWidth + 'px',
-      height: sourceHeight + 'px'
-    });
+    var $cards = container.find(".card").toArray();
+    console.log("test")
 
 
+    if (isFullsize) {
 
-    $(".card").addClass("wait");
-    setTimeout(() => {
-        $(".card-container").animate({
-            scrollTop: card.offset().top - (container.offset().top + 20)
-        }, 800);
-    }, 900);
-    setTimeout(() => {
-        $(".card").removeClass("wait");
-    }, 900);
+        $($cards).each(function(index) {
+
+            if ($(this).attr("id") == card.attr("id")) {
+
+                $(this).removeClass("big");
+                container.removeClass("big");
+
+                setTimeout(() => {
+                    container.removeClass("active");
+                }, 1800);
+
+
+            } else {
+
+                $(this).removeClass("up");
+
+                setTimeout(() => {
+                    $(this).removeClass('left');
+                    $(this).removeClass('right');
+                }, 500);
+
+            }
+
+        });
+
+        setTimeout(() => {
+
+            $($cards).each(function(index) {
+                $(this).toggleClass("absolute");
+                removeStyle($(this));
+            });
+
+
+            container.scrollTop(scrollBefore);
+        }, 1800);
+
+    } else {
+
+        scrollBefore = container.scrollTop();
+        var positionList = []
+
+        $($cards).each(function(index) {
+           var position = $(this).position(); // Get position relative to the offset parent
+           var width = $(this).width(); // Get width of the element
+           var height = $(this).height(); // Get height of the element
+
+           // Save position, width, and height data into an object
+           var elementData = {
+               id: $(this).attr("id"),
+               position: position,
+               width: width,
+               height: height
+           };
+
+           positionList.push(elementData);
+        });
+
+        positionList.forEach(function(elementData) {
+
+            thisCard = container.find("#"+elementData.id);
+
+           thisCard.css({
+              position: 'absolute',
+              top: elementData.position.top + 'px',
+              left: elementData.position.left + 'px',
+              width: elementData.width + 'px',
+              height: elementData.height + 'px'
+            });
+
+            thisCard.toggleClass("absolute");
+        });
+
+        $($cards).each(function(index) {
+
+            if ($(this).attr("id") == card.attr("id")) {
+
+            setTimeout(() => {
+                $(this).addClass("big");
+                container.addClass("big");
+                container.addClass("active");
+            }, 100);
+
+
+            } else {
+
+                var className = (index % 2 === 0) ? 'left' : 'right';
+
+                // Add the determined class to the current div
+                $(this).addClass(className);
+
+                setTimeout(() => {
+                    $(this).addClass("up");
+                }, 800);
+
+            }
+
+        });
+
+    }
+
+
+
 }
 
 function turnAround(div) {
